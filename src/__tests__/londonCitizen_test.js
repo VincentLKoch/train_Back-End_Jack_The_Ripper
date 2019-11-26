1af7bc9232c62fae945a96d6f14dbebe6fd2e35f
Aadhar-Ag <55687136+Aadhar-Ag@users.noreply.github.com>
2019-11-26 17:55:07 +0100

    Create londonCitizen_test.js

---
+++ src/__tests__/londonCitizen_test.js
@@ -0,0 +1,14 @@
+import LondonCitizen from '../londonCitizen'
+describe('londonCItizen test', () => {
+
+    it('new citizen is made', async () => {
+
+            const londonCItizen = new LondonCitizen(1, "Toto", 1, 1, false )
+           
+            expect(londonCItizen.id).toBe(1)
+            expect(londonCItizen.name).toBe("Toto")
+            expect(londonCItizen.posX).toBe(1)
+            expect(londonCItizen.posY).toBe(1)
+            expect(londonCItizen.isVictim).toBe(false)
+        })
+})
