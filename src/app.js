c9f16d4c57b0372f6be46fd95802b2915dc76365
VincentLKoch <Vincent.lel.koch@gmail.com>
2019-11-28 17:32:13 +0100

    initialize api

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser'
 const app = express()
 
 app.use(bodyParser.json())
-app.use(function(_req, res, next) {
+app.use(function (_req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
   res.header(
@@ -14,6 +14,25 @@ app.use(function(_req, res, next) {
   next()
 })
 
-// TODO...
+
+app.post('/citizen/:name/:posX/:posY', (req, res) => {
+
+})
+
+app.post('/victim/:name/:posX/:posY', (req, res) => {
+
+})
+
+app.get('/getJack', (req, res) => {
+
+})
+
+app.delete('/evidences', (req, res) => {
+
+})
+
+
+
+
 
 export default app
