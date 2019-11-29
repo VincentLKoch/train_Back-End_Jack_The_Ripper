a42f61ab0c2af2880aff7534b6d87d5cbbd8e553
Aadhar-Ag <55687136+Aadhar-Ag@users.noreply.github.com>
2019-11-29 13:19:29 +0100

    Update London.js

--- src/London.js
+++ src/London.js
@@ -6,7 +6,6 @@ class London {
     constructor() {
         this.dal = new Dal()
         this.victimExists = false
-        this.numClosest = 0
     }
 
     async createCitizen(name, posX, posY) {
@@ -52,7 +51,9 @@ class London {
             const victim = seperated[0]
             const citizens = seperated[1]
             const closestIndex 
-            const distance 
+            const distance
+            numClosest = 0
+
             if (!victim) {throw "fin1"} // no victim found
 
             for (let index = 0; index < citizens.length; index++) {
@@ -67,20 +68,20 @@ class London {
 
                 if (index == 0){ 
                     distance = dist
-                    this.numClosest = 1
+                    numClosest = 1
                 }
 
                 else{
                     if(dist < distance){
                         distance = dist
-                        this.numClosest = 1
+                        numClosest = 1
                         closestIndex = index
                     }
-                    else if (dist = distance) {this.numClosest = this.numClosest + 1}
+                    else if (dist = distance) {numClosest = numClosest + 1}
                 }
             }
 
-        if (this.numClosest !== 1) {throw "fin2"} // more than one citizen is the closest 
+        if (numClosest !== 1) {throw "fin2"} // more than one citizen is the closest 
 
         return await citizens[closestIndex]
 
