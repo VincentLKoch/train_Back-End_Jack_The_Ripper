8889ffb4805bd5b2c66517b191b06004cd316055
VincentLKoch <Vincent.lel.koch@gmail.com>
2019-11-29 23:34:40 +0100

    fixing london.js

--- src/London.js
+++ src/London.js
@@ -23,8 +23,8 @@ class London {
             if (this.victimExists) { //already a victim
                 throw "vic1"
             }
-            
-            if ((victim.posX !== xPos)||( victim.posY !== yPos)) {
+
+            if ((victim.posX !== xPos) || (victim.posY !== yPos)) {
                 //Incorrect positions given
                 throw "vic2"
             }
@@ -50,12 +50,12 @@ class London {
             const seperated = await this.dal.seperateVictimAndCitizens()
             const victim = seperated[0]
             const citizens = seperated[1]
-            const closestIndex 
-            const distance
-            numClosest = 0
+            let closestIndex
+            let dist
+            let numClosest = 0
 
-            if (!victim) {throw "fin1"} // no victim found
-            if (!citizens){throw "fin2"}
+            if (!victim) { throw "fin1" } // no victim found
+            if (!citizens) { throw "fin2" }
 
             for (let index = 0; index < citizens.length; index++) {
                 const citizen = await citizens[index];
@@ -65,26 +65,26 @@ class London {
                 xDiff *= xDiff
                 yDiff *= yDiff
 
-                dist = Math.sqrt( xs + ys )
+                dist = Math.sqrt(xs + ys)
 
-                if (index == 0){ 
-                    distance = dist
+                if (index == 0) {
+                    dist = dist
                     numClosest = 1
                 }
 
-                else{
-                    if(dist < distance){
-                        distance = dist
+                else {
+                    if (dist < dist) {
+                        dist = dist
                         numClosest = 1
                         closestIndex = index
                     }
-                    else if (dist = distance) {numClosest = numClosest + 1}
+                    else if (dist = dist) { numClosest = numClosest + 1 }
                 }
             }
 
-        if (numClosest !== 1) {throw "fin3"} // more than one citizen is the closest 
+            if (numClosest !== 1) { throw "fin3" } // more than one citizen is the closest 
 
-        return await citizens[closestIndex]
+            return await citizens[closestIndex]
 
         } catch (error) {
             throw error
