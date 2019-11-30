cd859c679b652e5f0c19e44536c4f58d826d324f
VincentLKoch <Vincent.lel.koch@gmail.com>
2019-11-30 23:50:19 +0100

    create citizen

--- src/__tests__/app/citizen_test.js
+++ src/__tests__/app/citizen_test.js
@@ -1,136 +1,70 @@
 import request from 'supertest'
 import app from '../../app'
-
 import * as citizenDependency from '../../London'
 
-const NAME = 'Elias Parker'
-const POSX = 18
-const POSY = -2
-
-const POSX2 = 16
-const POSY2 = 0
-
-beforeEach(() => {
-    citizenDependency.getLondon = jest.fn().mockReturnValue({
-        createCitizen: jest.fn()
-    })
-})
-
 afterEach(() => {
-    jest.resetAllMocks()
+    jest.clearAllMocks()
 })
 
-describe('Citizen Tests', () => {
-    it('When data is fine', (done) => {
-        const query = {
-            name: NAME,
-            posX: POSX,
-            posY: POSY
-        }
+it('Working post', async () => {
+    const fakeResponse = { name: 'testName', posX: 10, posY: -12, iscitizen: true }
+    const createCitizen = jest.fn().mockReturnValue(fakeResponse)
 
-        const expectedResponseBody = {
-            LondonCitizen: {
-                id: 1,
-                name: NAME,
-                posX: "POSX",
-                posY: "POSY"
-            }
-        }
-
-        const createCitizen = jest.fn().mockReturnValue(expectedResponseBody)
-
-        citizenDependency.getLondon.mockReturnValue({
-            createCitizen
-        })
-
-        request(app)
-            .post('/citizen/name/123/456')
-            .query(query)
+    citizenDependency.getLondon = jest.fn().mockReturnValue({
+        createCitizen
+    })
+    try {
+        await request(app)
+            .post('/citizen/testName/10/-12')
             .expect(200)
             .expect('Content-Type', /json/)
             .expect(response => {
-                expect(response.body).toEqual(expectedResponseBody)
-                expect(createCitizen).toHaveBeenCalledTimes(1)
-                expect(createCitizen).toHaveBeenCalledWith(NAME, '' + POSX, '' + POSY)
+                expect(response.body).toEqual(fakeResponse)
             })
-            .end(done)
-    })
-
-
-    it('When PosX parameter is not a int', (done) => {
-        const query = {
-            name: NAME,
-            posX: POSX2,
-            posY: POSY
-        }
-
-        const expectedResponseBody = {
-            message: "Bad request, We need both posX and posY to be non-null integer",
-            receive: {
-                name: 'Elias Parker',
-                posX: 16,
-                posY: -2
-            }
-
-        }
-
-        const message = jest.fn().mockReturnValue(expectedResponseBody)
-        const receive = jest.fn().mockReturnValue(expectedResponseBody)
-        const createCitizen = jest.fn().mockReturnValue(expectedResponseBody)
-
-        citizenDependency.getLondon.mockReturnValue({
-            message,
-            receive,
-        })
-
-        request(app)
-            .post('/citizen/name/123/456')
-            .query(query)
-            .expect(400)
-            .expect('Content-Type', /json/)
-            .expect(response => {
-                expect(response.body).toEqual(expectedResponseBody)
-                expect(createCitizen).toHaveBeenCalledTimes(0)
-            })
-            .end(done)
-    })
-
-    it('When PosY parameter equal 0', (done) => {
-        const query = {
-            name: NAME,
-            posX: POSX,
-            posY: POSY2
-        }
-
-        const expectedResponseBody = {
-            message: "Bad request, We need both posX and posY to be non-null integer",
-            receive: {
-                name: 'Elias Parker',
-                posX: "18",
-                posY: "0"
-            }
-
-        }
-
-        const message = jest.fn().mockReturnValue(expectedResponseBody)
-        const receive = jest.fn().mockReturnValue(expectedResponseBody)
-        const createCitizen = jest.fn().mockReturnValue(expectedResponseBody)
-
-        citizenDependency.getLondon.mockReturnValue({
-            message,
-            receive,
-        })
-
-        request(app)
-            .post('/citizen/name/123/456')
-            .query(query)
-            .expect(400)
-            .expect('Content-Type', /json/)
-            .expect(response => {
-                expect(response.body).toEqual(expectedResponseBody)
-                expect(createCitizen).toHaveBeenCalledTimes(0)
-            })
-            .end(done)
-    })
-
+    } catch (err) {
+        expect(err).toBeNull()
+    }
+    expect(createCitizen).toHaveBeenCalledWith('testName', "10", "-12")
+    expect(createCitizen).toHaveBeenCalledTimes(1)
 })
+
+
+it.each(
+    [
+        ["posX not int", '/citizen/testName/ten/-12'],
+        ["posY not int", '/citizen/testName/10/twelve'],
+    ])(
+        'bad request: %s',
+        async (description, testURL) => {
+            const splittedURL = testURL.split('/')
+            const fakeResponse = {
+                message: "Bad request, We need both posX and posY to be non-null integer",
+                receive: {
+                    name: splittedURL[2],
+                    posX: splittedURL[3],
+                    posY: splittedURL[4],
+                }
+            }
+
+            const createCitizen = jest.fn().mockReturnValue(fakeResponse)
+            citizenDependency.getLondon = jest.fn().mockReturnValue({
+                createCitizen
+            })
+
+            let response
+            try {
+
+                response = await request(app).post(testURL)
+
+                expect(response.status).toBe(400)
+                expect(response.body).toEqual(fakeResponse)
+
+                //expect(response.type).toBe(/json/)
+            } catch (err) {
+                expect(err).toBeNull()
+            }
+
+            expect(createCitizen).toHaveBeenCalledTimes(0)
+
+
+        })
